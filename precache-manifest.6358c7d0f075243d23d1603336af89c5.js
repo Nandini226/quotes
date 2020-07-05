217bac48337d7c3215444e4c62c8912a3d180cae
Nandini226 <nandinijoshi1231@gmail.com>
2020-07-05 19:25:45 +0530

    Updates

---
+++ precache-manifest.6358c7d0f075243d23d1603336af89c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909c3b4e198b01799d39efa5f6ba49e3",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "e4320c2a686a4772d277",
+    "url": "/quotes/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "561a511dfc1bab8a94e0",
+    "url": "/quotes/static/css/main.9ec14ee7.chunk.css"
+  },
+  {
+    "revision": "e4320c2a686a4772d277",
+    "url": "/quotes/static/js/2.2c130187.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/quotes/static/js/2.2c130187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561a511dfc1bab8a94e0",
+    "url": "/quotes/static/js/main.334660b9.chunk.js"
+  },
+  {
+    "revision": "42135f8f194da03e2725",
+    "url": "/quotes/static/js/runtime-main.8b2ab2e9.js"
+  }
+]);
