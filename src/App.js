842cfbcbf65aa46184e2c2637a14c8416139a8e2
Nandini226 <nandinijoshi1231@gmail.com>
2020-07-05 19:22:52 +0530

    deploy my app to gh pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import * as firebase from 'firebase';
+function Apps() {
+  const [currentQuote, setCurrentQuote] = useState([]);
+  const [input, setInput] = useState();
+  const [value, setValue] = useState();
+
+
+
+  const handle = event => {
+    setInput(event.target.value);
+  }
+
+
+
+  let database = firebase.database();
+  let ref = database.ref("user");
+  ref.on("value", gotData, gotErr)
+  function gotData(data) {
+    console.log(data);
+  }
+  function gotErr(data) {
+    console.log("ERRO!", data);
+  }
+  const addToArray = e => {
+    e.preventDefault();
+    const db = firebase.firestore();
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    const userRef = db.collection('user').add({
+      quote: input
+    });
+    console.log("added", input)
+    setInput("");
+  }
+  let newQuoteList = [];
+  useEffect(() => {
+    const db = firebase.firestore();
+    db.collection('user').get().then((snapshot) => {
+      //let newQuoteList = [];
+      snapshot.docs.forEach(doc => {
+        newQuoteList.push(doc.data());
+      })
+      setCurrentQuote(newQuoteList);
+    })
+  }, [])
+  let arr = [];
+  const print =
+    currentQuote.map(quote => (
+      arr.push(quote.quote)
+
+    ))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <h1 className=" book text-center">Quote Generator</h1>
+      <div className="box text-center">
+        {currentQuote && (
+          <div className="box1 " id="box1">{arr[Math.floor(arr.length * Math.random())]}
+          </div>
+        )}
+      </div>
+
+      <div className="text-center class">
+        <h5>Type your Quote in the below input field</h5>
+      </div>
+      <input className="input" value={input} onChange={handle} />
+      <button className=" yo btn btn-primary" onClick={addToArray}>Add my quote</button>
+      
+
+
     </div>
   );
 }
 
-export default App;
+
+export default Apps;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import * as firebase from 'firebase';
+
+const  firebaseConfig = {
+  apiKey: "AIzaSyCZGmwVWWzgH-ST9bzQQC1sWaMqOzZOZbc",
+  authDomain: "quotes-80daa.firebaseapp.com",
+  databaseURL: "https://quotes-80daa.firebaseio.com",
+  projectId: "quotes-80daa",
+  storageBucket: "quotes-80daa.appspot.com",
+  messagingSenderId: "580506222775",
+  appId: "1:580506222775:web:0d79312a957585d4c8cd32"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
